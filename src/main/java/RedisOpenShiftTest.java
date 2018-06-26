bdc1b243208f40f2d21cc3988e92344779253f7e
Josh <josh.revell@ontario.ca>
2018-06-26 16:04:38 -0400

    another

--- src/main/java/RedisOpenShiftTest.java
+++ src/main/java/RedisOpenShiftTest.java
@@ -4,9 +4,12 @@ public class RedisOpenShiftTest {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("Test... can you see me?");
+		
 		Jedis jedis = new Jedis("redis://redis-ephemeral", 6379);
 		jedis.set("1", "Hello World!");
 		System.out.println(jedis.get("1"));
+		jedis.close();
 		
 	}
 
