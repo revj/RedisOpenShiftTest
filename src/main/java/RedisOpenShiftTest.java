bb3e037687d33f1a645a86c2c3210981156be3f9
Josh <josh.revell@ontario.ca>
2018-06-27 11:38:28 -0400

    dsffds

--- src/main/java/RedisOpenShiftTest.java
+++ src/main/java/RedisOpenShiftTest.java
@@ -1,20 +1,32 @@
-import com.lambdaworks.redis.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import redis.clients.jedis.Jedis;
 
 public class RedisOpenShiftTest {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		System.out.println("Test... can you see me?");
 		
-		RedisClient redisClient = new RedisClient(
-			      RedisURI.create("redis://redis:6379/"));
-			    RedisConnection<String, String> connection = redisClient.connect();
-
-			    System.out.println("Connected to Redis");
-
-			    connection.close();
-			    redisClient.shutdown();
-		
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Connect to 1. Peter's or 2. OpenShift's (1/2):");
+		String result = input.readLine();
+		if (result.equals("1")) {
+			
+			Jedis jedis = new Jedis("142.145.212.217", 6379);
+			jedis.set("1", "Hello World!");
+			System.out.println(jedis.get("1"));
+			jedis.close();
+			
+		}
+		else {
+			
+			Jedis jedis = new Jedis("redis://redis", 6379);
+			jedis.set("1", "Hello World!");
+			System.out.println(jedis.get("1"));
+			jedis.close();
+		}
 	}
-
 }
