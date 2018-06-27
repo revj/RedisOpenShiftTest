12b7ec861aa8d9cca54cfced63a970d7ea039b67
Josh <josh.revell@ontario.ca>
2018-06-27 11:43:04 -0400

    FDafddfs

--- src/main/java/RedisOpenShiftTest.java
+++ src/main/java/RedisOpenShiftTest.java
@@ -10,23 +10,9 @@ public class RedisOpenShiftTest {
 		
 		System.out.println("Test... can you see me?");
 		
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Connect to 1. Peter's or 2. OpenShift's (1/2):");
-		String result = input.readLine();
-		if (result.equals("1")) {
-			
-			Jedis jedis = new Jedis("142.145.212.217", 6379);
-			jedis.set("1", "Hello World!");
-			System.out.println(jedis.get("1"));
-			jedis.close();
-			
-		}
-		else {
-			
-			Jedis jedis = new Jedis("redis://redis", 6379);
-			jedis.set("1", "Hello World!");
-			System.out.println(jedis.get("1"));
-			jedis.close();
-		}
+		Jedis jedis = new Jedis("142.145.212.217", 6379);	// redis://redis:6379/
+		jedis.set("1", "Hello World!");
+		System.out.println(jedis.get("1"));
+		jedis.close();
 	}
 }
