57d746375d548ad453d9fa822f8be71a3683c7e0
Josh <josh.revell@ontario.ca>
2018-06-28 14:26:01 -0400

    using cluster ip

--- src/main/java/RedisOpenShiftTest.java
+++ src/main/java/RedisOpenShiftTest.java
@@ -10,7 +10,7 @@ public class RedisOpenShiftTest {
 		
 		System.out.println("Test... can you see me?");
 		
-		Jedis jedis = new Jedis("redis://redis", 6379);	// redis://redis:6379/
+		Jedis jedis = new Jedis("172.30.93.221", 6379);	// redis://redis:6379/
 		jedis.set("1", "Hello World!");
 		System.out.println(jedis.get("1"));
 		jedis.close();
